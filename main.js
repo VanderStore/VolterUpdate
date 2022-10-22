5b7866999df99f5860586794f91d918af58e042b
VanderStore <113655725+VanderStore@users.noreply.github.com>
2022-10-22 22:41:58 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 require('./config')
 const { default: volterConnect, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = require("@adiwajshing/baileys")
-const { state, saveState } = useSingleFileAuthState(`./session/${sessionName}.json`)
+const { state, saveState } = useSingleFileAuthState(`./${sessionName}.json`)
 const pino = require('pino')
 const { Boom } = require('@hapi/boom')
 const fs = require('fs')
